497b70f84f36193551e554a2e4b11bb959bf53ab
Rodrigo-Molero-1901 <ra.molerocaceda1901@gmail.com>
2024-07-01 11:16:16 -0500

    Setup dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
